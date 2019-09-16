98526ef8d8377d23f19052659b54dd68bd6ca45d
Justin K -School <justinjoseph0007@gmail.com>
2019-09-16 15:48:26 -0400

    Use linear layout instead of constraints

--- Lab1Calculator/app/src/main/java/com/example/lab1_calculator/MainActivity.java
+++ Lab1Calculator/app/src/main/java/com/example/lab1_calculator/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.lab1_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
