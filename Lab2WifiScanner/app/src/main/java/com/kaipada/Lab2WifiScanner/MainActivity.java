ed8271739f1ef1f3a69c305f407ede18c2f31a3c
Justin K -School <justinjoseph0007@gmail.com>
2019-09-18 16:38:01 -0400

    Good enough wifi scanner lmao

--- Lab2WifiScanner/app/src/main/java/com/kaipada/Lab2WifiScanner/MainActivity.java
+++ Lab2WifiScanner/app/src/main/java/com/kaipada/Lab2WifiScanner/MainActivity.java
@@ -1,14 +1,90 @@
 package com.kaipada.Lab2WifiScanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    WifiManager mWifiManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Permission is not granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, 5);
+        }
+
+        // Permission is not granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CHANGE_WIFI_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CHANGE_WIFI_STATE}, 6);
+        }
+
+        // Permission is not granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_WIFI_STATE}, 7);
+        }
+
+        // Permission is not granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, 8);
+        }
+
     }
+
+    private final BroadcastReceiver mWifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent)
+        {
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+            {
+                List<ScanResult> mScanResults = mWifiManager.getScanResults();
+                for (ScanResult result : mScanResults) {
+                    System.out.println();
+                    Log.d("BSSID", result.BSSID);
+                    Log.d("SSID: ",result.SSID);
+                    Log.d("Strength: ",Integer.toString(result.level));
+                    Log.d("Time: ",Long.toString(result.timestamp));
+                }
+            }
+        }
+    };
+
+    public void startMethod(View view){
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        registerReceiver(mWifiScanReceiver,
+                new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mWifiManager.startScan();
+    }
+
+    public void stopMethod(View view){
+        System.out.println("This is a test");
+    }
+
 }
