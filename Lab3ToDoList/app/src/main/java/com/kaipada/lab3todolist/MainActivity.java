6e80d81af06769524768b682e5395b513f48829a
Justin K -School <justinjoseph0007@gmail.com>
2019-09-25 14:55:30 -0400

    Adding initial non-working changes

---
+++ Lab3ToDoList/app/src/main/java/com/kaipada/lab3todolist/MainActivity.java
@@ -0,0 +1,24 @@
+package com.kaipada.lab3todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button newButton = (Button) findViewById(R.id.newButton);
+    ListView notesList = (ListView) findViewById(R.id.notesList);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+	ArrayList<String> titles = new ArrayList<>();
+	ArrayAdapter adapter = new ArrayAdapter<String>(context:this,andriod.R.layout.somple_list_item_1,titles);
+	notesList.setAdapter(adapter);
+    }
+}
+
+public void newNote(View v) {
+    
+}
