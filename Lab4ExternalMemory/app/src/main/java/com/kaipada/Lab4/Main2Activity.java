4f1f328f3ee7f8dbaf8a724af0646ffbfba616e8
Justin K -Linux <justinjoseph0007@gmail.com>
2019-10-02 16:05:02 -0400

    Implemented read functionalites as well

--- Lab4ExternalMemory/app/src/main/java/com/kaipada/Lab4/Main2Activity.java
+++ Lab4ExternalMemory/app/src/main/java/com/kaipada/Lab4/Main2Activity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -20,15 +26,58 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     public void privateShowMethod(View view){
-        Toast.makeText(getApplicationContext(),"Clicked on private",Toast.LENGTH_SHORT).show();
+        String data =null;
+        FileReader fileReader = null;
+        try {
+            // FileReader reads text files in the default encoding.
+            fileReader = new FileReader(getExternalFilesDir(null)+ "/private.txt");
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((data = bufferedReader.readLine()) != null) {
+                textView.setText(data); // Lol can only set one line, it will overwrite it everytime
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            Log.w("ExternalStorage", "File not found" + fileReader, ex);
+        }
+        catch(IOException ex) {
+            Log.w("ExternalStorage", "Error reading " + fileReader, ex);
+        }
     }
 
     public void publicShowMethod(View view){
-        Toast.makeText(getApplicationContext(),"Clicked on public",Toast.LENGTH_SHORT).show();
+        String data =null;
+        FileReader fileReader = null;
+        try {
+            // FileReader reads text files in the default encoding.
+            fileReader = new FileReader(Environment.getExternalStorageDirectory()+ "/public.txt");
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((data = bufferedReader.readLine()) != null) {
+                textView.setText(data); // Lol can only set one line, it will overwrite it everytime
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            Log.w("ExternalStorage", "File not found" + fileReader, ex);
+        }
+        catch(IOException ex) {
+            Log.w("ExternalStorage", "Error reading " + fileReader, ex);
+        }
     }
 
     public void backMethod(View view){
         Intent intent = new Intent(this, MainActivity.class); // go back
         startActivity(intent);
     }
+
 }
