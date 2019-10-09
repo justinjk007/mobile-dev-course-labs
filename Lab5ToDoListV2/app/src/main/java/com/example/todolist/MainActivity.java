fbb0dbd71070db4bbe2f3482d188fbda79e081e4
Justin K -Linux <justinjoseph0007@gmail.com>
2019-10-09 14:59:06 -0400

    Save the scroll location when user changes screen orientation

--- Lab5ToDoListV2/app/src/main/java/com/example/todolist/MainActivity.java
+++ Lab5ToDoListV2/app/src/main/java/com/example/todolist/MainActivity.java
@@ -20,34 +20,49 @@ public class MainActivity extends AppCompatActivity {
     Button newNoteB;
     ListView notesList;
     NotesDB notes;
+    int index, top = 0;		// save scroll positions for the list
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        newNoteB = (Button) findViewById(R.id.newNoteBtn);
-        notesList = (ListView) findViewById(R.id.notesList);
-        ArrayList<String> titles = new ArrayList<>();
-        notes = new NotesDB(this);
-        titles = notes.getTitles();
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,titles);
-        notesList.setAdapter(adapter);
-        notesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String title = (String) parent.getItemAtPosition(position);
-                Intent intent = new Intent(MainActivity.this,ViewNote.class);
-                intent.putExtra("title",title);
-                intent.putExtra("old","old");
-                int REQUEST_ID=13;
-                startActivityForResult(intent,REQUEST_ID);
-            }
-        });
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+	newNoteB = (Button) findViewById(R.id.newNoteBtn);
+	notesList = (ListView) findViewById(R.id.notesList);
+	ArrayList<String> titles = new ArrayList<>();
+	notes = new NotesDB(this);
+	titles = notes.getTitles();
+	ArrayAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,titles);
+	notesList.setAdapter(adapter);
+	notesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		    String title = (String) parent.getItemAtPosition(position);
+		    Intent intent = new Intent(MainActivity.this,ViewNote.class);
+		    intent.putExtra("title",title);
+		    intent.putExtra("old","old");
+		    int REQUEST_ID=13;
+		    startActivityForResult(intent,REQUEST_ID);
+		}
+	    });
+	if (savedInstanceState != null) {
+	    // If the savedInstanceState is not null then activity was made before so reset list scroll position
+	    notesList.setSelectionFromTop(index, top);
+	}
 
     }
+
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+	super.onSaveInstanceState(outState);
+	// save index and top position
+	index = notesList.getFirstVisiblePosition();
+	View v = notesList.getChildAt(0);
+	top = (v == null) ? 0 : (v.getTop() - notesList.getPaddingTop());
+    }
+
     public void newNote(View v)
     {
-        Intent intent = new Intent(this, Note.class);
-        startActivity(intent);
+	Intent intent = new Intent(this, Note.class);
+	startActivity(intent);
     }
 }
 
